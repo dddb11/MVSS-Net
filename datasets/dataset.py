495581f3d4da17630942de2be3cabf34ed62d55a
sunnyhaze <mxch1122@126.com>
2023-01-12 23:45:56 +0800

    [update] fixed all bugs and able to train!

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,15 +1,81 @@
 import random
 import numpy as np
 import cv2
-
 from torch.utils.data import Dataset
-
+import torch
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
-
+from torch.nn import functional as F
 from albumentations.core.transforms_interface import DualTransform
 
 
+class Edge_generator(torch.nn.Module):
+    """generate the 'edge bar' for a 0-1 mask Groundtruth of a image
+    Algorithm is based on 'Morphological Dilation and Difference Reduction'
+    
+    Which implemented with fixed-weight Convolution layer with weight matrix looks like a cross,
+    for example, if kernel size is 3, the weight matrix is:
+        [[0, 1, 0],
+        [1, 1, 1],
+        [0, 1, 0]]
+
+    """
+    def __init__(self, kernel_size = 3) -> None:
+        super().__init__()
+        self.kernel_size = kernel_size
+    
+    def _dilate(self, image, kernel_size=3):
+        """Doings dilation on the image
+
+        Args:
+            image (_type_): 0-1 tensor in shape (B, C, H, W)
+        """
+        assert kernel_size % 2 == 1, "Kernel size must be odd"
+        assert image.shape[2] > kernel_size and image.shape[3] > kernel_size, "Image must be larger than kernel size"
+        
+        kernel = torch.zeros((1, 1, kernel_size, kernel_size))
+        kernel[0, 0, kernel_size // 2: kernel_size//2+1, :] = 1
+        kernel[0, 0, :,  kernel_size // 2: kernel_size//2+1] = 1
+        kernel = kernel.float()
+        # print(kernel)
+        res = F.conv2d(image, kernel.view([1,1,kernel_size, kernel_size]),stride=1, padding = kernel_size // 2)
+        return (res > 0) * 1.0
+
+
+    def _find_edge(self, image, kernel_size=3, return_all=False):
+        """Find 0-1 edges of the image
+
+        Args:
+            image (_type_): 0-1 ndarray in shape (B, C, H, W)
+        """
+        image = image.clone().float()
+        shape = image.shape
+        
+        if len(shape) == 2:
+            image = image.reshape([1, 1, shape[0], shape[1]])
+        if len(shape) == 3:
+            image = image.reshape([1, shape[0], shape[1], shape[2]])   
+        assert image.shape[1] == 1, "Image must be single channel"
+        
+        img = self._dilate(image, kernel_size=kernel_size)
+        
+        erosion = self._dilate(1-image, kernel_size=kernel_size)
+
+        diff = -torch.abs(erosion - img) + 1
+        diff = (diff > 0) * 1.0
+        # res = dilate(diff)
+        diff = diff.numpy()
+        if return_all :
+            return diff, img, erosion
+        else:
+            return diff
+    
+    def forward(self, x, return_all=False):
+        """
+        Args:
+            image (_type_): 0-1 ndarray in shape (B, C, H, W)
+        """
+        return self._find_edge(x, self.kernel_size, return_all=return_all)
 
 class RandomCopyMove(DualTransform):
     def __init__(self,
@@ -67,7 +133,6 @@ class RandomCopyMove(DualTransform):
             c_pos_w: c_pos_w + c_window_w, 
             : 
         ]
-        print(copy_region.shape)
         image[
             self.p_pos_h : self.p_pos_h + self.p_window_h,
             self.p_pos_w : self.p_pos_w + self.p_window_w,
@@ -83,8 +148,6 @@ class RandomCopyMove(DualTransform):
     
         manipulated_region =  np.full((self.p_window_h, self.p_window_w), 1)
         img = img.copy()
-        print(manipulated_region.shape)
-        print(self.p_window_w, self.p_window_h)
         img[
             self.p_pos_h : self.p_pos_h + self.p_window_h,
             self.p_pos_w : self.p_pos_w + self.p_window_w,
@@ -128,7 +191,6 @@ class RandomInpainting(DualTransform):
     def apply(self, img: np.ndarray, **params) -> np.ndarray:
         img = img.copy()
         img = np.uint8(img)
-        print(img.dtype)
         H, W, C = img.shape
         mask = np.zeros((H, W), dtype=np.uint8)
         # inpainting region
@@ -137,9 +199,7 @@ class RandomInpainting(DualTransform):
             self.pos_h : self.pos_h+ self.window_h,
             self.pos_w : self.pos_w + self.window_w,
         ] = 1
-        print(mask.shape)
         inpaint_flag = cv2.INPAINT_TELEA if random.random() > 0.5 else cv2.INPAINT_NS
-        print(inpaint_flag)
         img = cv2.inpaint(img, mask, 3,inpaint_flag)
         return img
     def apply_to_mask(self, img: np.ndarray, **params) -> np.ndarray:
@@ -290,6 +350,7 @@ class DeepfakeDataset(Dataset):
             A.Resize(self.image_size // 4, self.image_size // 4), # specially for edge mask, as the paper uses H/4 and W/4
             ToTensorV2()
         ])
+        self.edge_generator =  Edge_generator(kernel_size= 15)
 
     def __getitem__(self, item):
         # ----------
@@ -325,24 +386,27 @@ class DeepfakeDataset(Dataset):
 
         random.seed(seed)
         if (not self.val):
-            input = self.transform_train(image = input)['image']
+            res = self.transform_train(image = input, mask = mask)
+            input = res['image']
+            mask = res['mask']
         else:
             input = self.transform_val(image = input)['image']
-        input = input / 255.0
 
         random.seed(seed)
         if (not self.val):
-            mask = self.transform_train(image = mask)['image']
+            pass
         else:
             mask = self.transform_val(image = mask)['image']
         mask = mask / 255.0
 
         random.seed(seed)
         if (not self.val):
-            edge = self.transform_train_edge(image = edge)['image']
+            edge = self.edge_generator((mask>0.5) * 1.0)[0][0]
+            edge = self.transform_val_edge(image = edge)['image']
+            
         else:
             edge = self.transform_val_edge(image = edge)['image']
-        edge = edge / 255.0
+            edge = edge / 255.0
 
         return input, mask, edge, self.labels[item]
 
@@ -353,7 +417,6 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     img = plt.imread("/root/workspace/MVSS/CASIAv1/TP/Sp/Sp_D_CND_A_pla0005_pla0023_0281.jpg")
     mask = cv2.imread("/root/workspace/MVSS/CASIAv1/Gt/Sp_D_CND_A_pla0005_pla0023_0281_gt.png", cv2.IMREAD_GRAYSCALE)
-    print(np.max(mask))
     plt.subplot(1, 4, 1)
     plt.imshow(img)
     plt.subplot(1, 4 ,2)
